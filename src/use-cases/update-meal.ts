d91e8b7fe8399f8b3dac7e9bb5c142a0bad079b3
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-18 10:46:25 -0300

    feat: add update meal use case

---
+++ src/use-cases/update-meal.ts
@@ -0,0 +1,50 @@
+import { MealRepository } from "@/repositories/meal-repository";
+import { Meal } from "@prisma/client";
+
+interface UpdateMealUseCaseRequest {
+  mealId: string;
+  userId: string;
+  name: string;
+  description: string;
+  dateTime: Date;
+  isOnDiet: boolean;
+}
+
+interface UpdateMealUseCaseResponse {
+  meal: Meal;
+}
+
+export class UpdateMealUseCase {
+  constructor(private mealRepository: MealRepository) {}
+
+  async execute({
+    mealId,
+    userId,
+    name,
+    isOnDiet,
+    description,
+    dateTime,
+  }: UpdateMealUseCaseRequest): Promise<UpdateMealUseCaseResponse> {
+    const meal = await this.mealRepository.findById(mealId)
+
+    if (!meal) {
+      throw new Error('Meal not found');
+    }
+
+    if(meal.user_id !== userId) {
+        throw new Error('Unauthorized');
+    }
+
+    const updateMeal = await this.mealRepository.update(mealId, {
+      name,
+      description,
+      date_time: dateTime,
+      is_on_diet: isOnDiet,
+      user_id: userId,
+    });
+
+    return {
+      meal: updateMeal,
+    };
+  }
+}
