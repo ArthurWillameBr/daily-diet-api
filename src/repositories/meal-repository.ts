78346c9435cdca4e2baf950dcd6f99f229fa08fd
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-18 10:46:43 -0300

    feat: add update meal repository

--- src/repositories/meal-repository.ts
+++ src/repositories/meal-repository.ts
@@ -2,4 +2,6 @@ import { Meal, Prisma } from "@prisma/client";
 
 export interface MealRepository {
     create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
+    update(id: string, data: Prisma.MealUncheckedUpdateInput): Promise<Meal>
+    findById(id: string): Promise<Meal | null>
 }
