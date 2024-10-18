debf6573a03b7e53fc72aeda757421bf2d1bacd6
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-18 09:44:59 -0300

    feat: add register use case

---
+++ src/use-cases/register.ts
@@ -0,0 +1,41 @@
+import { UsersRepository } from "@/repositories/users-repository";
+import { User } from "@prisma/client";
+import { hash } from "bcryptjs";
+
+interface RegisterUseCaseRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface RegisterUseCaseResponse {
+  user: User;
+}
+
+export class RegisterUseCase {
+  constructor(private usersRepository: UsersRepository) {}
+
+  async execute({
+    name,
+    email,
+    password,
+  }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
+    const password_hash = await hash(password, 6);
+
+    const userWithEmail = await this.usersRepository.findByEmail(email);
+
+    if (userWithEmail) {
+      throw new Error("User already exists");
+    }
+
+    const user = await this.usersRepository.create({
+      name,
+      email,
+      password_hash,
+    });
+
+    return {
+      user,
+    };
+  }
+}
