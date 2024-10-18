01c075fa0979fabe2106aaa1e1ae08661ef36345
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-18 10:54:22 -0300

    feat: add delete meal method to MealRepository

--- src/repositories/meal-repository.ts
+++ src/repositories/meal-repository.ts
@@ -3,5 +3,6 @@ import { Meal, Prisma } from "@prisma/client";
 export interface MealRepository {
     create(data: Prisma.MealUncheckedCreateInput): Promise<Meal>
     update(id: string, data: Prisma.MealUncheckedUpdateInput): Promise<Meal>
+    delete(id: string): Promise<Meal>
     findById(id: string): Promise<Meal | null>
 }
