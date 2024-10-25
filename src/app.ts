367527b73f0164958816a3c91e761b8d1d86de5a
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-25 15:00:23 -0300

    feat: add jwt token for authenticate

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,16 @@
 import fastify from "fastify";
 import { routes } from "./http/controllers/routes";
+import fastifyJwt from "@fastify/jwt";
+import { env } from "./env";
 
 export const app = fastify()
+
+app.register(fastifyJwt, {
+    secret: env.JWT_SECRET,
+    sign: {
+        expiresIn: "12h"
+    }
+},
+)
 
 app.register(routes)
--- src/http/controllers/authenticate.ts
+++ src/http/controllers/authenticate.ts
@@ -4,26 +4,41 @@ import { InvalidCredentialsError } from "@/use-cases/errors/invalid-credentials-
 import { FastifyReply, FastifyRequest } from "fastify";
 import { z } from "zod";
 
-export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
-    const authenticateBodySchema = z.object({
-        email: z.string().email(),
-        password: z.string().min(6)
-    })
-    const { email, password } = authenticateBodySchema.parse(request.body)
+export async function authenticate(
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const authenticateBodySchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(6),
+  });
+  const { email, password } = authenticateBodySchema.parse(request.body);
 
-    try {
-        const userRepository = new PrismaUserRepository()
-        const authenticateUseCase = new AuthenticateUseCase(userRepository)
+  try {
+    const userRepository = new PrismaUserRepository();
+    const authenticateUseCase = new AuthenticateUseCase(userRepository);
 
-        await authenticateUseCase.execute({
-            email, 
-            password
-        })
-    } catch (error) {
-        if(error instanceof InvalidCredentialsError) {
-            return reply.status(400).send({message: error.message})
-        }
-        throw error
+    const { user } = await authenticateUseCase.execute({
+      email,
+      password,
+    });
+
+    const token = reply.jwtSign(
+      {},
+      {
+        sign: {
+          sub: user.id,
+        },
+      }
+    );
+
+    return reply.status(200).send({
+      token,
+    });
+  } catch (error) {
+    if (error instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: error.message });
     }
-    return reply.status(200).send({message: "User authenticated"})
+    throw error;
+  }
 }
