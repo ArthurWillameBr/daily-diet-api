27cc486b2d99c99be921199e1603fcf3b4191478
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-26 22:30:34 -0300

    feat: add create-meal controller

---
+++ src/http/controllers/create-meal.ts
@@ -0,0 +1,30 @@
+import { PrismaMealRepository } from "@/repositories/prisma/prisma-meal-repository";
+import { CreateMealUseCase } from "@/use-cases/create-meal";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function CreateMeal(request: FastifyRequest, reply: FastifyReply) {
+  const createMealBodySchema = z.object({
+    name: z.string(),
+    description: z.string().nullable(),
+    dateTime: z.date(), //Conferir isso aqui depois
+    isOnDiet: z.boolean(),
+  });
+
+  const { name, description, dateTime, isOnDiet } = createMealBodySchema.parse(
+    request.body
+  );
+
+  const mealRepository = new PrismaMealRepository();
+  const createMealUseCase = new CreateMealUseCase(mealRepository);
+
+  await createMealUseCase.execute({
+    userId: request.user.sub,
+    name,
+    description,
+    dateTime,
+    isOnDiet,
+  });
+
+  return reply.status(201).send({ message: "Meal created successfully" });
+}
