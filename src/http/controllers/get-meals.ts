93a2aec3b8d043e3790991285b5c00f1ab41e134
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-30 15:32:31 -0300

    feat: add get-meals controller to retrieve meals for authenticated users

---
+++ src/http/controllers/get-meals.ts
@@ -0,0 +1,12 @@
+import { PrismaMealRepository } from "@/repositories/prisma/prisma-meal-repository";
+import { GetMealsUseCase } from "@/use-cases/get-meals";
+import { FastifyReply, FastifyRequest } from "fastify";
+
+export async function GetMeals(request: FastifyRequest, reply: FastifyReply) {
+  const mealsRepository = new PrismaMealRepository();
+  const getMealsUseCase = new GetMealsUseCase(mealsRepository);
+
+  const { meals } = await getMealsUseCase.execute({ userId: request.user.sub });
+
+  return reply.status(200).send({ meals });
+}
