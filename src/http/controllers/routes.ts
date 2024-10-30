d61e0e0a36f6d9bf14a01bce257eebd3720f52aa
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-30 16:16:31 -0300

    feat: add route to get total meals outside diet for authenticated users

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -6,6 +6,7 @@ import { VerifyJWT } from "../middlewares/verify-jwt";
 import { GetMeals } from "./get-meals";
 import { GetMealsById } from "./get-meals-by-id";
 import { GetUserTotalMealsWithinDiet } from "./get-user-total-meals-within-diet";
+import { GetUserTotalMealsOutsideDiet } from "./get-user-total-meals-outside-diet";
 
 export async function routes(app: FastifyInstance) {
     app.post("/users", register)
@@ -15,4 +16,5 @@ export async function routes(app: FastifyInstance) {
     app.get("/meals", {onRequest: VerifyJWT}, GetMeals)
     app.get("/meals/:mealId", {onRequest: VerifyJWT}, GetMealsById)
     app.get("/meals/within-diet", {onRequest: VerifyJWT}, GetUserTotalMealsWithinDiet)
+    app.get("/meals/outside-diet", {onRequest: VerifyJWT}, GetUserTotalMealsOutsideDiet)
 }
