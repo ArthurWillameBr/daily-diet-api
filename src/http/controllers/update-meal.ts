d3c0f921419f4d06046028a7f8c39f78a1e094ac
arthurwillame5 <arthurwillame2005@gmail.com>
2024-10-31 09:05:27 -0300

    feat: add controller to update meals for authenticated users

---
+++ src/http/controllers/update-meal.ts
@@ -0,0 +1,34 @@
+import { PrismaMealRepository } from "@/repositories/prisma/prisma-meal-repository";
+import { UpdateMealUseCase } from "@/use-cases/update-meal";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from "zod";
+
+export async function UpdateMeal(request: FastifyRequest, reply: FastifyReply) {
+  const UpdateMealParamsSchema = z.object({
+    mealId: z.string().uuid(),
+  });
+  const UpdateMealBodySchema = z.object({
+    name: z.string(),
+    description: z.string().nullable(),
+    dateTime: z.date(),
+    isOnDiet: z.boolean(),
+  });
+
+  const { name, description, dateTime, isOnDiet } = UpdateMealBodySchema.parse(
+    request.body
+  );
+  const { mealId } = UpdateMealParamsSchema.parse(request.params);
+
+  const mealRepository = new PrismaMealRepository();
+  const updateMeal = new UpdateMealUseCase(mealRepository);
+
+  const { meal } = await updateMeal.execute({
+    userId: request.user.sub,
+    mealId,
+    name,
+    description,
+    dateTime,
+    isOnDiet,
+  });
+  return reply.status(200).send({ meal });
+}
