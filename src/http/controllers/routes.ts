94f2e34c38cce438f7e7f8deae2df692f45cc2e7
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-16 20:17:46 -0300

    feat: add get last registered meals route

--- src/http/controllers/routes.ts
+++ src/http/controllers/routes.ts
@@ -9,16 +9,30 @@ import { GetUserTotalMealsWithinDiet } from "./get-user-total-meals-within-diet"
 import { GetUserTotalMealsOutsideDiet } from "./get-user-total-meals-outside-diet";
 import { GetUserTotalMeals } from "./get-user-total-meals";
 import { UpdateMeal } from "./update-meal";
+import { GetLastRegisteredMeals } from "./get-last-registered-meals";
 
 export async function routes(app: FastifyInstance) {
-    app.post("/users", register)
-    app.post("/sessions", authenticate)
+  app.post("/users", register);
+  app.post("/sessions", authenticate);
 
-    app.post("/meals", {onRequest: VerifyJWT}, CreateMeal)
-    app.get("/meals", {onRequest: VerifyJWT}, GetMeals)
-    app.get("/meals/:mealId", {onRequest: VerifyJWT}, GetMealsById)
-    app.put("/meals/:mealId/update", {onRequest: VerifyJWT}, UpdateMeal)
-    app.get("/meals/within-diet", {onRequest: VerifyJWT}, GetUserTotalMealsWithinDiet)
-    app.get("/meals/outside-diet", {onRequest: VerifyJWT}, GetUserTotalMealsOutsideDiet)
-    app.get("/meals/total", {onRequest: VerifyJWT}, GetUserTotalMeals)
+  app.post("/meals", { onRequest: VerifyJWT }, CreateMeal);
+  app.get("/meals", { onRequest: VerifyJWT }, GetMeals);
+  app.get("/meals/:mealId", { onRequest: VerifyJWT }, GetMealsById);
+  app.put("/meals/:mealId/update", { onRequest: VerifyJWT }, UpdateMeal);
+  app.get(
+    "/meals/within-diet",
+    { onRequest: VerifyJWT },
+    GetUserTotalMealsWithinDiet
+  );
+  app.get(
+    "/meals/outside-diet",
+    { onRequest: VerifyJWT },
+    GetUserTotalMealsOutsideDiet
+  );
+  app.get("/meals/total", { onRequest: VerifyJWT }, GetUserTotalMeals);
+  app.get(
+    "/meals/last-registered",
+    { onRequest: VerifyJWT },
+    GetLastRegisteredMeals
+  );
 }
