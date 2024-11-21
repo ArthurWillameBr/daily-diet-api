fe7964d7517f219aa794c191153cdad17e0a9208
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-21 10:38:22 -0300

    chore: cors config

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import fastify from "fastify";
 import { routes } from "./http/controllers/routes";
 import fastifyJwt from "@fastify/jwt";
 import { env } from "./env";
+import cors from "@fastify/cors";
 
-export const app = fastify()
+export const app = fastify();
+
+app.register(cors, {
+  origin: "*",
+});
 
 app.register(fastifyJwt, {
-    secret: env.JWT_SECRET,
-    sign: {
-        expiresIn: "12h"
-    }
-},
-)
+  secret: env.JWT_SECRET,
+  sign: {
+    expiresIn: "12h",
+  },
+});
 
-app.register(routes)
+app.register(routes);
