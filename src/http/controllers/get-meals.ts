393a0dcd3b5e593e31a537009636715d13815ffb
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-22 17:20:26 -0300

    feat: enhance GetMealsUseCase to group and format meals by date

--- src/http/controllers/get-meals.ts
+++ src/http/controllers/get-meals.ts
@@ -8,12 +8,5 @@ export async function GetMeals(request: FastifyRequest, reply: FastifyReply) {
 
   const { meals } = await getMealsUseCase.execute({ userId: request.user.sub });
 
-  const mealsWithHiddenUserId = meals.map((meal) => ({
-    ...meal,
-    user_id: undefined,
-  }));
-
-  return reply.status(200).send({
-    meals: mealsWithHiddenUserId,
-  });
+  return reply.status(200).send({ meals });
 }
--- src/use-cases/get-meals.ts
+++ src/use-cases/get-meals.ts
@@ -1,22 +1,65 @@
 import { MealRepository } from "@/repositories/meal-repository";
-import { Meal } from "@prisma/client";
 
 interface GetMealsUseCaseRequest {
-    userId: string
+  userId: string;
+}
+
+interface FormattedMeal {
+  time: string;
+  name: string;
+  isOnDiet: boolean;
 }
 
 interface GetMealsUseCaseResponse {
-    meals: Meal[]
+  meals: Array<{
+    date: string;
+    meals: FormattedMeal[];
+  }>;
 }
 
 export class GetMealsUseCase {
-    constructor(private mealRepository: MealRepository) {}
+  constructor(private mealRepository: MealRepository) {}
 
-    async execute({userId}: GetMealsUseCaseRequest): Promise<GetMealsUseCaseResponse> {
-        const meals = await this.mealRepository.findAllByUserId(userId)
+  async execute({
+    userId,
+  }: GetMealsUseCaseRequest): Promise<GetMealsUseCaseResponse> {
+    const meals = await this.mealRepository.findAllByUserId(userId);
 
-        return {
-            meals
-        }
-    } 
+    // Agrupar por data
+    const groupedMeals = meals.reduce((acc, meal) => {
+      const date = new Date(meal.date_time).toLocaleDateString("pt-BR");
+      const time = new Date(meal.date_time).toLocaleTimeString("pt-BR", {
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+
+      acc[date].push({
+        time,
+        name: meal.name,
+        isOnDiet: meal.is_on_diet,
+      });
+
+      return acc;
+    }, {} as Record<string, FormattedMeal[]>);
+
+    // Ordenar as datas de forma decrescente
+    const sortedDates = Object.keys(groupedMeals).sort((a, b) => {
+      const dateA = new Date(a.split("/").reverse().join("-"));
+      const dateB = new Date(b.split("/").reverse().join("-"));
+      return dateB.getTime() - dateA.getTime(); // Mais recente primeiro
+    });
+
+    const formattedMeals = sortedDates.map((date) => ({
+      date,
+      meals: groupedMeals[date],
+    }));
+
+    return {
+      meals: formattedMeals,
+    };
+  }
 }
