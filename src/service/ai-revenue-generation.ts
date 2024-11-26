100fa0986aac05038b431e33473a955681c79bb7
ArthurWillameBr <arthurwillame2005@gmail.com>
2024-11-26 09:38:22 -0300

    feat: update AI model and enhance recipe generation prompt with dynamic diet and cuisine selection

--- src/service/ai-revenue-generation.ts
+++ src/service/ai-revenue-generation.ts
@@ -5,7 +5,7 @@ import { env } from "@/env";
 import { z } from "zod";
 
 export class AiRevenueGeneration {
-  private model = groq("llama3-70b-8192");
+  private model = groq("llama-3.1-70b-versatile");
 
   constructor() {
     if (!process.env.GROQ_API_KEY) {
@@ -17,8 +17,32 @@ export class AiRevenueGeneration {
   }
 
   async reportGeneration() {
+    const diets = [
+      "vegetariana",
+      "vegana",
+      "sem glúten",
+      "cetogênica",
+      "paleo",
+    ];
+    const cuisines = [
+      "italiana",
+      "japonesa",
+      "mexicana",
+      "indiana",
+      "mediterrânea",
+    ];
+    const randomDiet = diets[Math.floor(Math.random() * diets.length)];
+    const randomCuisine = cuisines[Math.floor(Math.random() * cuisines.length)];
+
+    const dynamicPrompt = `Gere uma receita saudável e diversificada. Considere a culinária ${randomCuisine} e a dieta ${randomDiet}. 
+      Evite ingredientes comuns e garanta criatividade. Certifique-se de não repetir ingredientes frequentemente usados.
+      **Responda em pt-BR.**`;
+
     const result = await generateObject({
       model: this.model,
+      temperature: 1,
+      topK: 50,
+      topP: 0.9,
       schema: z.object({
         recipe: z.object({
           name: z.string(),
@@ -31,12 +55,8 @@ export class AiRevenueGeneration {
           steps: z.array(z.string()),
         }),
       }),
-      prompt:
-        "Gere uma receita saudável e diversificada que considere diferentes tipos de dietas (vegetariana, vegana, sem glúten, cetogênica, paleo) e diversas culinárias ao redor do mundo (italiana, japonesa, mexicana, indiana, mediterrânea). Certifique-se de incluir uma variedade de ingredientes e métodos de preparo para evitar repetições. A receita deve ser criativa, inovadora e saborosa. **Responda sempre em pt-BR**",
+      prompt: dynamicPrompt,
     });
-
-    console.log(result);
-
     return result.object.recipe;
   }
 }
